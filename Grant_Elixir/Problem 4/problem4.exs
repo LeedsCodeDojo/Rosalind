b14d8193a9e064b0c893821a4008338f2ac20e0c
Grant Crofton <grantac@hotmail.com>
2016-01-05 23:11:28 +0000

    fixed problem 4

--- Grant_Elixir/Problem 4/problem4.exs
+++ Grant_Elixir/Problem 4/problem4.exs
@@ -9,9 +9,9 @@ input =
 defmodule Problem do
     def fib(n,k) do
         case n do
-            1 -> k
-            2 -> k
-            _ -> fib(n-1, k) + fib(n-2, k)
+            1 -> 1
+            2 -> 1
+            _ -> fib(n-1, k) + fib(n-2, k) * k
         end
     end
 end
